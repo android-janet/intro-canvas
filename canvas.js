41abe3200408cb800d89f529270b4f8712922db3
Melissa Khat <melissa.khat@gmail.com>
2015-05-06 10:50:37 -0600

    blue on-click, red dbl-click

--- canvas.js
+++ canvas.js
@@ -1,3 +1,4 @@
+
 function drawBorder(){
    var canvas = document.getElementById("canvas");
    var context = canvas.getContext('2d');
@@ -6,3 +7,21 @@ function drawBorder(){
 }
 
 drawBorder();
+
+
+function redRectangle(){
+   var canvas = document.getElementById("canvas");
+   var context = canvas.getContext('2d');
+   var size = {x: canvas.width, y: canvas.height};
+   context.fillStyle='red';
+   context.fillRect(size.x/2, size.y/2,size.x, size.y);
+}
+
+
+function blueRectangle(){
+   var canvas = document.getElementById("canvas");
+   var context = canvas.getContext('2d');
+   var size = {x:canvas.width, y:canvas.height};
+   context.fillStyle='blue';
+   context.fillRect(0,0, size.x/2, size.y/2);
+}
