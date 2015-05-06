b6822917f6efd19dae8e6a1df9eb704cacf008a9
Melissa Khat <melissa.khat@gmail.com>
2015-05-06 09:53:19 -0600

    draw canvas

--- canvas.js
+++ canvas.js
@@ -1,6 +1,8 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
-// }
+function drawBorder(){
+   var canvas = document.getElementById("canvas");
+   var context = canvas.getContext('2d');
+   var size = { x: canvas.width, y: canvas.height };
+   context.strokeRect(0,0, size.x, size.y);
+}
+
+drawBorder();
