599224092094105dc3c5e0b1c7316040e333b64e
Melissa Khat <melissa.khat@gmail.com>
2015-05-06 11:25:01 -0600

    button functions, add random squares

--- canvas.js
+++ canvas.js
@@ -25,3 +25,44 @@ function blueRectangle(){
    context.fillStyle='blue';
    context.fillRect(0,0, size.x/2, size.y/2);
 }
+
+
+function redButton(){
+   var canvas = document.getElementById("canvas");
+   var context = canvas.getContext('2d');
+   var size = {x:canvas.width, y:canvas.height};
+   context.fillStyle='red';
+   context.fillRect(Math.floor(Math.random()*100), Math.floor(Math.random()*100),
+                Math.floor(Math.random()*200), Math.floor(Math.random()*200));
+
+
+}
+
+function greenButton(){
+   var canvas = document.getElementById("canvas");
+   var context = canvas.getContext('2d');
+   var size = {x:canvas.width, y:canvas.height};
+   context.fillStyle='green';
+   context.fillRect(Math.floor(Math.random()*100), Math.floor(Math.random()*100),
+                Math.floor(Math.random()*200), Math.floor(Math.random()*200));
+}
+
+function blueButton(){
+   var canvas = document.getElementById("canvas");
+   var context = canvas.getContext('2d');
+   var size = {x:canvas.width, y:canvas.height};
+   context.fillStyle='blue';
+   context.fillRect(Math.floor(Math.random()*100), Math.floor(Math.random()*100),
+                Math.floor(Math.random()*200), Math.floor(Math.random()*200));
+
+}
+
+function orangeButton(){
+   var canvas = document.getElementById("canvas");
+   var context = canvas.getContext('2d');
+   var size = {x:canvas.width, y:canvas.height};
+   context.fillStyle='orange';
+   context.fillRect(Math.floor(Math.random()*100), Math.floor(Math.random()*100),
+                Math.floor(Math.random()*200), Math.floor(Math.random()*200));
+
+}
